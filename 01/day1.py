8d4282fb5f72fd3a3d37cf594c708b1faa5064ed
Julius Roth <jrot@osb-ag.de>
2021-12-01 17:55:31 +0100

    Added solutions for day 1.

--- 01/day1.py
+++ 01/day1.py
@@ -1,7 +1,29 @@
 from util.fileReader import readNumbers
 
-data = readNumbers(open("input"))
-print(data)
-print(len(data))
+formatted_input = readNumbers(open("input"))
+sample = readNumbers(open("sample"))
 
 
+# part 1
+def count_increases(data):
+    increases = 0
+    for i in range(len(data) - 1):
+        if data[i] < data[i + 1]:
+            increases += 1
+    return increases
+
+
+# solution part 1:
+print(count_increases(formatted_input))
+
+
+# part 2
+def generate_windows(data):
+    increase_windows = []
+    for i in range(len(data) - 2):
+        increase_windows.append(data[i] + data[i + 1] + data[i + 2])
+    return increase_windows
+
+
+# solution part 2:
+print(count_increases(generate_windows(formatted_input)))
